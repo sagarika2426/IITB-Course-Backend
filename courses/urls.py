b1fa728fe1f10374512ed857b1df3fa1741de5c8
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2024-08-23 01:08:20 +0530

    urls for instances and courses

--- courses/urls.py
+++ courses/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import (
     CourseListCreateAPIView, CourseDetailAPIView,
-    CourseInstanceListCreateAPIView, CourseInstanceDetailAPIView
+    CourseInstanceListCreateAPIView, CourseInstanceDetailAPIView, AllCourseInstancesAPIView
 )
 
 urlpatterns = [
     path('courses/', CourseListCreateAPIView.as_view(), name='course-list-create'),
     path('courses/<int:pk>/', CourseDetailAPIView.as_view(), name='course-detail'),
-    path('instances/<int:year>/<int:semester>/', CourseInstanceListCreateAPIView.as_view(), name='instance-list-create'),
+    path('instances/', CourseInstanceListCreateAPIView.as_view(), name='instance-list-create'),
+    path('instances/<int:year>/<int:semester>/', CourseInstanceListCreateAPIView.as_view(), name='instance-list-view'),
     path('instances/<int:year>/<int:semester>/<int:course_id>/', CourseInstanceDetailAPIView.as_view(), name='instance-detail'),
 ]
+
