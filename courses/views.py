1171bc1d21cb6c47158b39f07834318bad7b9ee8
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2024-08-23 01:09:02 +0530

    modified CourseInstanceListCreateAPIView

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .models import Course, CourseInstance
+from rest_framework import serializers
 from .serializers import CourseSerializer, CourseInstanceSerializer
+from rest_framework.exceptions import NotFound
+
 
 class CourseListCreateAPIView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
@@ -14,22 +17,37 @@ class CourseInstanceListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = CourseInstanceSerializer
 
     def get_queryset(self):
-        year = self.kwargs['year']
-        semester = self.kwargs['semester']
-        return CourseInstance.objects.filter(year=year, semester=semester)
+        return CourseInstance.objects.all()
 
     def perform_create(self, serializer):
-        course_id = self.request.data.get('course_id')
-        course = Course.objects.get(id=course_id)
-        serializer.save(course=course)
+        course_title = self.request.data.get('course_title')
+        if course_title:
+            try:
+                course = Course.objects.get(title=course_title)
+            except Course.DoesNotExist:
+                raise serializers.ValidationError({"course_title": "Course with this title does not exist."})
+            serializer.save(course=course)
+        else:
+            raise serializers.ValidationError({"course_title": "This field is required."})
 
 class CourseInstanceDetailAPIView(generics.RetrieveDestroyAPIView):
     queryset = CourseInstance.objects.all()
     serializer_class = CourseInstanceSerializer
 
     def get_object(self):
-        return CourseInstance.objects.get(
-            course_id=self.kwargs['course_id'],
-            year=self.kwargs['year'],
-            semester=self.kwargs['semester']
-        )
+        course_id = self.kwargs['course_id']
+        year = self.kwargs['year']
+        semester = self.kwargs['semester']
+        print(f"Deleting instance with Course ID: {course_id}, Year: {year}, Semester: {semester}")
+        try:
+            return CourseInstance.objects.get(
+                course_id=course_id,
+                year=year,
+                semester=semester
+            )
+        except CourseInstance.DoesNotExist:
+            raise NotFound(detail="CourseInstance matching query does not exist.", code=404)
+    
+class AllCourseInstancesAPIView(generics.ListAPIView):
+    queryset = CourseInstance.objects.all()
+    serializer_class = CourseInstanceSerializer
